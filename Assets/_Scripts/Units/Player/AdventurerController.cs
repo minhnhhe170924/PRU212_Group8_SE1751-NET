440bcd498a9dbfa274b55dbca52c4ca424dab41b
Huy Nguyễn <huynguyen287123@gmail.com>
2024-10-10 00:39:46 +0700

    feat: adding idle, walking and running scripts and animations

--- Assets/_Scripts/Units/Player/AdventurerController.cs
+++ Assets/_Scripts/Units/Player/AdventurerController.cs
@@ -1,10 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class AdventurerController : PlayerUnitBase {
+public class AdventurerController : PlayerUnitBase
+{
     //[SerializeField] private AudioClip _someSound;
 
     //void Start() {
@@ -20,26 +22,101 @@ public class AdventurerController : PlayerUnitBase {
     //}
 
     public float walkSpeed = 5.0f;
+    public float runSpeed = 10.0f;
+
+    public float CurrentMoveSpeed
+    {
+        get
+        {
+            if (IsMoving)
+            {
+                if (IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
 
     Vector2 moveInput;
-    
-    public bool IsMoving { get; private set; }
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("IsMoving", value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool("IsRunning", value);
+        }
+    }
+
+    [SerializeField]
+    private bool _isFacingRight = true;
+
+    public bool IsFacingRight
+    {
+        get
+        {
+            return _isFacingRight;
+        }
+        private set
+        {
+            if (_isFacingRight != value)
+            {
+                // Flip the direction of player
+                this.transform.localScale *= new Vector2(-1, 1);
+            }
+            _isFacingRight = value;
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        
+
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -48,6 +125,32 @@ public class AdventurerController : PlayerUnitBase {
 
         IsMoving = moveInput != Vector2.zero;
 
+        SetFacingDirection(moveInput);
+    }
 
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            // Face to the right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            // Face to the left
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
