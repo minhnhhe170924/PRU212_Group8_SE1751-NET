d04eee33da572a00b507440d7994c8379b18a4b7
Huy Nguyễn <huynguyen287123@gmail.com>
2024-10-25 00:44:01 +0700

    fix: isGrounded check direction to depend on gravitySwitched or not

--- Assets/_Scripts/Units/Player/TouchingDirections.cs
+++ Assets/_Scripts/Units/Player/TouchingDirections.cs
@@ -101,7 +101,7 @@ public class TouchingDirections : MonoBehaviour
 
     private void FixedUpdate()
     {
-        IsGrounded = touchingCol.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
+        IsGrounded = touchingCol.Cast(groundCheckDirection, castFilter, groundHits, groundDistance) > 0;
         IsOnWall = touchingCol.Cast(wallCheckDirection, castFilter, wallHits, wallDistance) > 0;
         IsOnCeiling = touchingCol.Cast(ceilingCheckDirection, castFilter, ceilingHits, ceilingDistance) > 0;
     }
